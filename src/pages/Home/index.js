a0875770b289d94be72d00a1e9acad346c9ef41e
unknown <vlsf2@cin.ufpe.br>
2021-12-04 18:45:25 -0300

    ADD: JSON Rank acidentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,19 +8,39 @@ import { collection, getDocs } from 'firebase/firestore';
 
 const Home = (props) => {
 
-    const [de, setDe]   = useState(0);
-    const [ate, setAte] = useState(0);
+    const [de, setDe]   = useState('2021-01-01');
+    const [ate, setAte] = useState('2021-12-01');
     const caminhoCollection = collection(db, "acidentes_teste");
+    
     async function getAcidentes()  {
         // get acidentes
         const data = await getDocs(caminhoCollection);
-        const d = []
+        var d = []
         // map pra extrair dados
-        console.log('----')
-        data.forEach((doc) => {console.log(doc.id, '=>', doc.data()); d.push(doc.data)});
+        data.forEach((doc) => {
+            //doc.id
+            d.push(doc.data())
+        });
         return d
     };
 
+    async function sumAcidentes() {
+        const data = await getAcidentes();
+        var result = []
+        data.reduce(function(dic, acd){
+            if(!dic[acd.bairro]){
+                dic[acd.bairro] = {bairro: acd.bairro, qtd: 0};
+                result.push(dic[acd.bairro])
+            }
+            dic[acd.bairro].qtd += 1;
+            return dic
+        }, {});
+        result.sort(function(a,b) {return a.qtd > b.qtd ? -1 : a.qtd > b.qtd ? 1 : 0});
+        return result
+    }
+
+    //const data = getAcidentes();
+    const totalAcd = sumAcidentes();
     const propsTo = {
         pathname: `/bairros/${de}&${ate}`,
     }
@@ -36,7 +56,7 @@ const Home = (props) => {
                     <input className='date' type = "date" id = "ate" name="ate" value = {ate} onChange={(e) => setAte(e.target.value)} required/>
                 </div>
                 <div>
-                    <Button className='button' type = "submit" onClick= {() => console.log(getAcidentes())}>
+                    <Button className='button' type = "submit" onClick= {() => console.log(totalAcd)}>
                         <Link to={propsTo}><strong>Buscar</strong></Link>
                     </Button>
                 </div>
