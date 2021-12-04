bb4cf96bad6fb80e4db1b7f8b64020091ff95984
jpveloso0 <46942172+jpveloso0@users.noreply.github.com>
2021-12-04 00:55:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import { db } from "./services/firebase";
+import {
+    collection,
+    getDocs,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+} from "firebase/firestore";
+
+import './index.css';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './Routes'
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+    return ( 
+        <BrowserRouter className = "App" >
+            <Header titulo = { "Acidentes de Trânsito Recife/PE" }/> 
+            <Routes/>
+            <Footer desc = { 'Página desenvolvida utilizando ReactJs & NodeJS' }/>
+        </BrowserRouter>
+    )
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render( <App/> , rootElement)
