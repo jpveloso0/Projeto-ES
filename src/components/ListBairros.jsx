801a57d7e9ffa856ca7ef7164180d4946fe68389
unknown <vlsf2@cin.ufpe.br>
2021-12-07 00:20:50 -0300

    ADD: atributos do bairro

--- src/components/ListBairros.jsx
+++ src/components/ListBairros.jsx
@@ -13,7 +13,43 @@ function ListBairros(props) {
     if (data) {
       data.reduce(function (dic, acd) {
         if (!dic[acd.bairro]) {
-          dic[acd.bairro] = { bairro: acd.bairro, qtd: 0 };
+          dic[acd.bairro] = { bairro: acd.bairro
+                              , qtd: 0
+                              , periodo_manha: 0 //05:00 - 11:59
+                              , periodo_tarde: 0 //12:00 - 17:59
+                              , periodo_noite: 0 //18:00 - 23:59
+                              , periodo_madrugada: 0 //00:00 - 04:59
+                              , c_vitima: 0 //natureza_acidente
+                              , vitima_fatal: 0 //nataureza_acidente
+                              , tipo_alagamento: 0 //tipo
+                              , tipo_atropelamento: 0 //atropelamento & atropelamento pessoa
+                              , tipo_capotamento: 0 //tipo
+                              , tipo_choque: 0 //tipo
+                              , tipo_colisao: 0 // com ciclista & frorntal & lateral & transversal & traseira
+                              , tipo_engavetamento: 0 //tipo
+                              , tipo_queda: 0 //tipo
+                              , tipo_tombamento: 0 //tipo
+                              , qtd_auto: 0
+                              , qtd_moto: 0
+                              , qtd_ciclom: 0
+                              , qtd_ciclista: 0
+                              , qtd_pedestre: 0
+                              , qtd_onibus: 0
+                              , qtd_caminhao: 0
+                              , qtd_viatura: 0
+                              , qtd_outros: 0
+                              , qtd_vitimas: 0
+                              , qtd_vitimasfatais: 0
+                              , local_longo_via: 0 //acidente_verificado
+                              , local_cruzamento: 0 //acidente_verificado
+                              , tempo_bom: 0 //tempo_clima
+                              , tempo_chuvoso: 0 //tempo_clima
+                              , pista_seca: 0 //condicao_via
+                              , pista_molhada: 0 //condicao_via
+                              , pista_perfeita: 0 //conservação_via
+                              , pista_mal_cons: 0 //conservação_via
+                              , pista_mal_ilum: 0 //conservação_via
+                            };
           result.push(dic[acd.bairro]);
         }
         dic[acd.bairro].qtd += 1;
@@ -44,7 +80,7 @@ function ListBairros(props) {
       {currentData.length > 0 ? (
         currentData.map((item, index) => (
           <ListGroupItem action key={index} id="bairro" tag="button" onClick={() => selectedData(item)}>
-            {item.bairro} - {item.qtd} ACIDENTES
+            {`${index+1}º`} {item.bairro} - {item.qtd} ACIDENTES
           </ListGroupItem>
         ))
       ) : (
--- src/pages/Bairros/index.js
+++ src/pages/Bairros/index.js
@@ -1,6 +1,4 @@
 import './index.css';
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React, { useState, useEffect} from "react";
