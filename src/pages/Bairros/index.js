05d69cf70dbe4a094e64a82a2d6bb97b9a63949c
unknown <vlsf2@cin.ufpe.br>
2021-12-22 19:33:46 -0300

    ADD: início deploy

--- src/pages/Bairros/index.js
+++ src/pages/Bairros/index.js
@@ -17,7 +17,7 @@ const Bairros = () => {
     const [acidentes, setAcidentes] = useState();
     const [bairroAtual, setBairroAtual] = useState(null);
 
-    const caminhoCollection = collection(db, "acidentesfull");
+    const caminhoCollection = collection(db, "acidentes_teste");
 
     async function getAcidentes()  {
         // get acidentes
